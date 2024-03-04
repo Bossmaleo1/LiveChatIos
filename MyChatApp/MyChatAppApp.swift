e86abeae8d4556d348e47c2563c9e51969288de8
Bossmaleo1 <bossprogrammaleo@gmail.com>
2024-03-04 04:29:46 +0100

    Initial Commit

---
+++ MyChatApp/MyChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyChatAppApp.swift
+//  MyChatApp
+//
+//  Created by Sidney MALEO on 04/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
